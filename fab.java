aa2f6ee98eda8a04cc70e307b4d8237546631da2
Ashutosh-510 <113826784+Ashutosh-510@users.noreply.github.com>
2022-10-09 11:12:47 +0530

    some loop problems

---
+++ fab.java
@@ -0,0 +1,23 @@
+package ashutosh;
+
+import java.util.Scanner;
+
+public class fab {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= a) {
+            int temp = i;
+            b = b+a;
+            b = temp;
+            count++;
+        }
+        System.out.println(i);
+
+    }
+
+
+}
---
+++ forloop.java
@@ -0,0 +1,13 @@
+package ashutosh;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int num = 1; num<= 5; num += 1){
+            System.out.println(num);
+        }
+
+    }
+}
